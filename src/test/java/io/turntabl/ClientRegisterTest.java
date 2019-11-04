3198314d18eb03f99a9a484f4da19b7e52a8a57a
Dilyen <davidandohacquah@gmail.com>
2019-11-04 17:45:07 +0000

    Testing

--- src/test/java/io/turntabl/ClientRegisterTest.java
+++ src/test/java/io/turntabl/ClientRegisterTest.java
@@ -7,11 +7,11 @@ public class ClientRegisterTest {
     void getClientName() {
     }
     List<Client> Clients = Arrays.asList(
-            new Private("Quay", 101, ServiceLevel.GOLD),
+            new Private("Harrison", 101, ServiceLevel.GOLD),
             new Private("Dawud", 113, ServiceLevel.PLATINUM),
-            new Private("Jorginno", 350, ServiceLevel.PREMIUM),
-            new Private("Sam", 011, ServiceLevel.GOLD),
-            new Corporate("Debylynn",498,ServiceLevel.GOLD),
+            new Private("Robert", 350, ServiceLevel.PREMIUM),
+            new Private("Grace", 011, ServiceLevel.GOLD),
+            new Corporate("Debby",498,ServiceLevel.GOLD),
             new Corporate("kareen",543,ServiceLevel.PLATINUM),
             new Corporate("Daviv",999,ServiceLevel.PLATINUM),
             new Corporate("Dilyen",321,ServiceLevel.PLATINUM)
@@ -21,18 +21,6 @@ public class ClientRegisterTest {
 
     @Test
     void getGoldClients() {
-        List<Client> Clients = Arrays.asList(
-                new Private("Quay", 045, ServiceLevel.GOLD),
-                new Private("Dawud", 730, ServiceLevel.PLATINUM),
-                new Private("Jorginno", 3990, ServiceLevel.PREMIUM),
-                new Private("Sam", 400, ServiceLevel.GOLD),
-                new Corporate("Debylynn",498,ServiceLevel.GOLD),
-                new Corporate("kareen",543,ServiceLevel.PLATINUM),
-                new Corporate("Daviv",909,ServiceLevel.PLATINUM),
-                new Corporate("Dilyen",221,ServiceLevel.PLATINUM)
-
-        );
-
     }
 
 }
